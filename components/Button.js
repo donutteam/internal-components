dcdcfa8671c1a242fc2af95e6ce0dfeb23486ec0
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2022-07-20 11:29:51 -0400

    Updating Button to use tw- prefix.

--- components/Button.js
+++ components/Button.js
@@ -92,14 +92,14 @@ export function Button(options)
 		{
 			case "font-awesome":
 			default:
-				icon = new DE("span", `fa-fw ${ options.iconName } mr-2`);
+				icon = new DE("span", `fa-fw ${ options.iconName } tw-mr-2`);
 
 				break;
 
 			case "image":
 				icon = new DE("img",
 					{
-						class: "fa-fw mr-2", // Kind of a HACK
+						class: "fa-fw tw-mr-2", // Kind of a HACK
 						src: options.iconName,
 						alt: "Button Icon",
 					});
@@ -113,12 +113,12 @@ export function Button(options)
 			class:
 			[
 				...options.classes,
-				"inline-block",
-				"p-2",
-				"rounded-xl",
-				"bg-primary",
-				"hover:bg-secondary",
-				"text-white",
+				"tw-inline-block",
+				"tw-p-2",
+				"tw-rounded-xl",
+				"tw-bg-primary",
+				"tw-hover:tw-bg-secondary",
+				"tw-text-white",
 			],
 			href: options.href,
 			target: options.href != null ? options.target : null,
@@ -127,6 +127,6 @@ export function Button(options)
 			options.iconPosition == "before" ? icon : null,
 			options.text != null ? new DE("span", null, options.text) : null,
 			options.iconPosition == "after" ? icon : null,
-			options.external ? new DE("span", "fa-fw fa-thin fa-arrow-up-right-from-square ml-2") : null,
+			options.external ? new DE("span", "fa-fw fa-thin fa-arrow-up-right-from-square tw-ml-2") : null,
 		]);
 }
